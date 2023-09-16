c7473373e28b1ae7632059b7b2f75c4cac9006e1
Cat Adams <catcorbin0919@gmail.com>
2023-09-16 00:32:55 -0600

    creating mysql connection

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 //We need inquirer and mysql to use this application
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+
+//creating mysql connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "trilogy123",
+    database: "employeeTracker_db",
+});
